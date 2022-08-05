431e3f2d1f97f12a25f677197038b97304bd5384
gpdoud <gpdoud@gmail.com>
2022-08-05 12:35:03 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.ClearProviders().AddDebug();
+
+var webapi = builder.Configuration["ConfigSettings:Webapi"];
+var connstr = builder.Configuration["ConfigSettings:ConnStr"];
+
+// Add services to the container.
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.Logger.LogWarning($"webapi is {webapi}");
+app.Logger.LogInformation($"connstr is {connstr}");
+
+app.Run();
