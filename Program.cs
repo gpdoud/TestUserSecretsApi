712ad7125502f01e792409ed22c63d68f19f9704
gpdoud <gpdoud@gmail.com>
2022-08-06 12:46:02 -0400

    Final commit

--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
 
-builder.Logging.ClearProviders().AddDebug();
+builder.Logging.ClearProviders().AddDebug().SetMinimumLevel(LogLevel.Trace);
 
-var webapi = builder.Configuration["ConfigSettings:Webapi"];
-var connstr = builder.Configuration["ConfigSettings:ConnStr"];
+var webapi = builder.Configuration["AppSettings:Webapi"];
+var connstr = builder.Configuration["AppSettings:ConnStr"];
+var dbuser = builder.Configuration["AppSettings:DbUser"];
+var dbpassword = builder.Configuration["AppSettings:DbPassword"];
+var connectionStr = new Microsoft.Data.SqlClient.SqlConnectionStringBuilder(connstr);
+connectionStr.UserID = dbuser;
+connectionStr.Password = dbpassword;
+connectionStr.TrustServerCertificate = true;
 
 // Add services to the container.
 
@@ -12,6 +18,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 
 app.Logger.LogWarning($"webapi is {webapi}");
-app.Logger.LogInformation($"connstr is {connstr}");
+app.Logger.LogTrace($"connstr is {connstr}");
+app.Logger.LogInformation($"connection string is {connectionStr.ToString()}");
 
 app.Run();
